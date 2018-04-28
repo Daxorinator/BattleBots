081119515f4c66328eba8d49be9fe5e8a2b230b8
FrictionlessPortals <8077147+FrictionlessPortals@users.noreply.github.com>
2018-04-28 15:35:22 +0100

    Implemented Rudder for Left and Right Controls, also simplified functions for turning and forward/backward movements.

--- BattleBots.ino
+++ BattleBots.ino
@@ -1,76 +1,103 @@
-const int dZone = 35;
-int th = 0; // Start with Throttle at 0
-int thpin = 2; // Throttle Pin
-int el = 0; // Start with Elevator at 0
-int elpin = 3; // Elevator Pin
-int M1_F = 5; //Motor 1 Forward Pin
-int M2_F = 10; // Motor 2 Forward Pin
-int M1_B = 6; // Motor 1 Backward Pin
-int M2_B = 9; // Motor 2 Backward Pin
+int M1_F = 2; // Motor 1 Forward Pin
+int M1_B = 3; // Motor 1 Backward Pin
+int M2_F = 4; // Motor 2 Forward Pin
+int M2_B = 5; // Motor 2 Backward Pin
+
+int thpin = 6; // Throttle Pin
+int rupin = 7; // Rudder Pin
+
+const int dZone = 50;
 
 void setup() {
-    pinMode(M1_F, OUTPUT);
-    pinMode(M1_B, OUTPUT);
-    pinMode(M2_F, OUTPUT);
-    pinMode(M2_B, OUTPUT);
-    pinMode(thpin, INPUT);
-    pinMode(elpin, INPUT);
-    Serial.begin(9600);
-}
 
-void forward(int sp, int rl) {
-//    Serial.print("forward  M1=");
-//    Serial.print(sp+rl);
-//    Serial.print("   M2=");
-//    Serial.print(sp-rl);
-//    Serial.println();
-    analogWrite(M1_F, sp+rl);
-    analogWrite(M2_F, sp-rl);
-    analogWrite(M1_B, 0);
-    analogWrite(M2_B, 0);
-}
+  // Setup pins and motors
 
-void back(int sp, int rl) {
-//    Serial.print("Backward  M1=");
-//    Serial.print(sp+rl);
-//    Serial.print("   M2=");
-//    Serial.print(sp-rl);
-//    Serial.println();
-    analogWrite(M1_F, 0);
-    analogWrite(M2_F, 0);
-    analogWrite(M1_B, sp-rl);
-    analogWrite(M2_B, sp+rl);
+  pinMode(M1_F, OUTPUT);
+  pinMode(M1_B, OUTPUT);
+  pinMode(M2_F, OUTPUT);
+  pinMode(M2_B, OUTPUT);
+
+  pinMode(thpin, INPUT);
+  pinMode(rupin, INPUT);
+
+  Serial.begin(9600);
 }
 
 void stop() {
-    analogWrite(M1_F, 0);
-    analogWrite(M2_F, 0);
+
+  // Set motors to complete zero.
+
+  Serial.println("Stopping Motors");
+
+  analogWrite(M1_F, 0);
+  analogWrite(M1_B, 0);
+  analogWrite(M2_F, 0);
+  analogWrite(M2_B, 0);
+}
+
+void driveLR(int left, int right) {
+
+  Serial.print("Drive LEFT: ");
+  Serial.print(left);
+  Serial.print("  \t RIGHT: ");
+  Serial.print(right);
+  Serial.println();
+
+  if (left > 0) {
+    analogWrite(M1_F, left);
     analogWrite(M1_B, 0);
+  }
+  else
+  {
+    analogWrite(M1_F, 0);
+    analogWrite(M1_B, -left);
+  }
+
+  if (right > 0) {
+    analogWrite(M2_F, right);
     analogWrite(M2_B, 0);
+  }
+  else
+  {
+    analogWrite(M2_F, 0);
+    analogWrite(M2_B, -right);
+  }
+}
+
+int rxmap(int x)
+{
+  // If I get 0, return -999.
+  // Otherwise, throttle and others in range 1000-2000: map them to -255 to +255
+  
+  if (x == 0) return -999;
+  return (map(x, 1000, 2000, -255, 255));
 }
 
 void loop() {
-    th = pulseIn(thpin, HIGH, 25000); //Read Throttle Position
-    el = pulseIn(elpin, HIGH, 25000); //Read Eleavtor Position
 
-    Serial.println("Before map");
-    Serial.println(th); //Debug: Print th
-    Serial.println(el); //Debug: Print el
+  delay(500); // delay in between reads for stability
 
-    th = map(th, 1065, 1946, -255, 255); //Map throttle to -255, 255 for use in forward and back
-    el = map(el, 1947, 1028, -255, 255); //Map elevator to -255, 255 for use in left and right
+  int timeout = 35000;
+  int th = 0; // Start with Throttle at 0
+  int ru = 0; // Start with Rudder at 0
 
-    Serial.println("After map");
-    Serial.println(th); //Debug: Print th
-    Serial.println(el); //Debug: Print el
+  th = pulseIn(thpin, HIGH, timeout); //Read Throttle Position
+  ru = pulseIn(rupin, HIGH, timeout); //Read Rudder Position
 
-    delay(100); // delay in between reads for stability
-    
-    if (th > dZone) { //th > dzone would be above deadzone or pointing up
-      forward(th, el); //so go forward
-    } else if (th < -dZone) { //otherwise check if its less than the deadzone, which would be down
-      back(-th, -el); //then go backward negative th, to provide a positive output to use in function
-    } else { //otherwise, if it's in the deadzone
-      stop();  //stop moving
-    } 
+  th = rxmap(th);
+  ru = rxmap(ru);
+
+  Serial.print("After map: Th = ");
+  Serial.print(th); //Debug: Print th
+  Serial.print("   \t El = ");
+  Serial.print(ru); //Debug: Print ru
+  Serial.print("  \t\t");
+
+  if (th > -dZone && th < dZone && ru > -dZone && ru < dZone) {
+    stop();
+  }
+  else {
+    driveLR(th - ru, th + ru);
+  }
+
 }
